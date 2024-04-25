c6101af140586be9a37dfdbcd68c9a537a5ecd8d
EpicCodeLearner222 <136813735+EpicCodeLearner222@users.noreply.github.com>
2024-04-25 08:27:13 -0400

    Update leaderstats.lua

--- leaderstats.lua
+++ leaderstats.lua
@@ -11,13 +11,23 @@ local cash = Instance.new("NumberValue", leaderstats)
 cash.Name = "Cash"
 cash.Value = 0
 
+        local settings = Instance.new("Folder", plr)
+        settings.Name = "Settings"
+
+        local music = Instance.new("BoolValue", settings)
+        music.Name = "Music"
+        music.Value = false
+
 local data
+        local data2
     local success, fail = pcall(function()
 data = dataStore:GetAsync(plr.UserId.."-data")
+                data2 = dataStore:GetAsync(plr.UserId.."-music")
       end)
     if success then
       print("Got data successfully!")
       cash.Value = data
+            music.Value = data2
     else
 warn("Failed to get data")
 end)
